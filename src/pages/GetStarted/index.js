cccfb34fcb53f684962a5ee63fff1712b81a8a78
Bulu507 <53290382+Bulu507@users.noreply.github.com>
2020-11-23 21:17:31 +0700

    Sign Up/ Register

---
+++ src/pages/GetStarted/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {ILLogo, ILGetStarted} from '../../assets';
+import {Button, Gap} from '../../components';
+
+export default function GetStarted({navigation}) {
+  return (
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah dan flexible
+        </Text>
+      </View>
+      <View>
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate('Register')}
+        />
+        <Gap height={16} />
+        <Button
+          type="secondary"
+          title="Sign In"
+          onPress={() => navigation.replace('Login')}
+        />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  title: {
+    fontSize: 28,
+    color: 'white',
+    marginTop: 91,
+    fontFamily: 'Nunito-SemiBold',
+  },
+});
---
+++ src/pages/Login/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from '../../assets';
+import {Input, Link, Button, Gap} from '../../components';
+
+export default function Login() {
+  return (
+    <View style={styles.page}>
+      <ILLogo />
+      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+      <Input label="Email Address" />
+      <Gap height={24} />
+      <Input label="Password" />
+      <Gap height={10} />
+      <Link title="Forgot my password" size={12} />
+      <Gap height={40} />
+      <Button title="Sign In" />
+      <Gap height={30} />
+      <Link title="Create new account" size={16} align="center" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {padding: 40, backgroundColor: 'white', flex: 1},
+  title: {
+    fontSize: 20,
+    fontFamily: 'Nunito-SemiBold',
+    color: '#112340',
+    marginTop: 40,
+    marginBottom: 40,
+    maxWidth: 180,
+  },
+});
